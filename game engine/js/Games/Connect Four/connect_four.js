ff8ac2a029dcf7961b1caab2e8b499bbe3b58ca6
Hussein Khaled <husseinkhaled733@gmail.com>
2023-04-30 11:20:31 +0300

    updating connect 4 controller

--- game engine/js/Games/Connect Four/connect_four.js
+++ game engine/js/Games/Connect Four/connect_four.js
@@ -4,8 +4,16 @@ class connect_four extends gameEngine{
   state
   constructor() {
     super();
+    this.state = {}
+    for (let i = 1; i <= 6; i++) {
+      (this.state)[i] = [];
+      for (let j = 1; j <= 7; j++) {
+        (this.state)[i][j] = '';
+      }
+    }
     this.connectFourController = new connect_four_controller(this.connectFourDrawer, this.state)
     this.connectFourDrawer = new connect_four_drawer(this.connectFourController, this.state)
+    this.connectFourController.drawer = this.connectFourDrawer
   }
 
   init(){
