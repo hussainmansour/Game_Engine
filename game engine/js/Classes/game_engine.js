7f1748eb163005a536542f7460120a55f6ea87cb
Hussein Khaled <husseinkhaled733@gmail.com>
2023-05-05 20:59:42 +0300

    taking input

--- game engine/js/Classes/game_engine.js
+++ game engine/js/Classes/game_engine.js
@@ -5,19 +5,31 @@ class gameEngine{
     this.game()
   }
 
-  game(){
+  async game(){
     console.log("Game started")
     console.log(this.state)
 
-    this.drawer(this.state)
-    const input_button = document.createElement('button')
-    input_button.id = 'input';input_button.textContent = "Give Input"
-    input_button.addEventListener('click',() => {
-      const input = prompt("Enter input")
-      if(input) this.controller(this.state, input)
-      this.drawer(this.state)
-    });
-    document.body.appendChild(input_button)
+    while(true){
+      this.drawer(this.state);
+      let p=new Promise((resolve)=>{
+        setTimeout(()=>{
+          let input = prompt("Enter input or click cancel to exit");
+          resolve(input);
+        },2000)
+      });
+      let input=await p;
+      console.log(input);
+      if(input==null) break;
+      this.controller(this.state,input);
+    }
+    // const input_button = document.createElement('button')
+    // input_button.id = 'input';input_button.textContent = "Give Input"
+    // input_button.addEventListener('click',() => {
+    //   const input = prompt("Enter input")
+    //   if(input) this.controller(this.state, input)
+    //   this.drawer(this.state)
+    // });
+    // document.body.appendChild(input_button)
   }
   drawer(state){}
   controller(state, input){}
