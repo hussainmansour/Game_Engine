140aa91d6978247d22baab97fcb19fd578fbdf6d
Osama Belal <obelal010@gmail.com>
2023-05-07 01:58:11 +0300

    Update chess input

--- game engine/js/Games/Chess/chess.js
+++ game engine/js/Games/Chess/chess.js
@@ -1,74 +1,61 @@
 class chess extends gameEngine{
-  chessDrawer
-  chessController
-  state
   constructor() {
-    super();
-    this.state = {
-      'a1': {name: 'rook', color: 'white', symbol: '\u2656'},
-      'b1': {name: 'knight', color: 'white', symbol: '\u2658'},
-      'c1': {name: 'bishop', color: 'white', symbol: '\u2657'},
-      'd1': {name: 'queen', color: 'white', symbol: '\u2655'},
-      'e1': {name: 'king', color: 'white', symbol: '\u2654'},
-      'f1': {name: 'bishop', color: 'white', symbol: '\u2657'},
-      'g1': {name: 'knight', color: 'white', symbol: '\u2658'},
-      'h1': {name: 'rook', color: 'white', symbol: '\u2656'},
+    const state = {}
+    for (let i = 0; i < 8; i++) {
+      state[i] = [];
+      for (let j = 0; j < 8; j++)
+        state[i][j] = '';
+    }
+    const namePieces = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook'];
+    const blackPieces = ['\u265C', '\u265E', '\u265D', '\u265B', '\u265A', '\u265D', '\u265E', '\u265C'];
+    const whitePieces = ['\u2656', '\u2658', '\u2657', '\u2655', '\u2654', '\u2657', '\u2658', '\u2656'];
 
-      'a2': {name: 'pawn', color: 'white', symbol: '\u2659'},
-      'b2': {name: 'pawn', color: 'white', symbol: '\u2659'},
-      'c2': {name: 'pawn', color: 'white', symbol: '\u2659'},
-      'd2': {name: 'pawn', color: 'white', symbol: '\u2659'},
-      'e2': {name: 'pawn', color: 'white', symbol: '\u2659'},
-      'f2': {name: 'pawn', color: 'white', symbol: '\u2659'},
-      'g2': {name: 'pawn', color: 'white', symbol: '\u2659'},
-      'h2': {name: 'pawn', color: 'white', symbol: '\u2659'},
-
-      'a3': {name: '', color: '', symbol: ''}, 'b3': {name: '', color: '', symbol: ''},
-      'c3': {name: '', color: '', symbol: ''}, 'd3': {name: '', color: '', symbol: ''},
-      'e3': {name: '', color: '', symbol: ''}, 'f3': {name: '', color: '', symbol: ''},
-      'g3': {name: '', color: '', symbol: ''}, 'h3': {name: '', color: '', symbol: ''},
-
-      'a4': {name: '', color: '', symbol: ''}, 'b4': {name: '', color: '', symbol: ''},
-      'c4': {name: '', color: '', symbol: ''}, 'd4': {name: '', color: '', symbol: ''},
-      'e4': {name: '', color: '', symbol: ''}, 'f4': {name: '', color: '', symbol: ''},
-      'g4': {name: '', color: '', symbol: ''}, 'h4': {name: '', color: '', symbol: ''},
-
-      'a5': {name: '', color: '', symbol: ''}, 'b5': {name: '', color: '', symbol: ''},
-      'c5': {name: '', color: '', symbol: ''}, 'd5': {name: '', color: '', symbol: ''},
-      'e5': {name: '', color: '', symbol: ''}, 'f5': {name: '', color: '', symbol: ''},
-      'g5': {name: '', color: '', symbol: ''}, 'h5': {name: '', color: '', symbol: ''},
-
-      'a6': {name: '', color: '', symbol: ''}, 'b6': {name: '', color: '', symbol: ''},
-      'c6': {name: '', color: '', symbol: ''}, 'd6': {name: '', color: '', symbol: ''},
-      'e6': {name: '', color: '', symbol: ''}, 'f6': {name: '', color: '', symbol: ''},
-      'g6': {name: '', color: '', symbol: ''}, 'h6': {name: '', color: '', symbol: ''},
-
-
-      'a7': {name: 'pawn', color: 'black', symbol: '\u265F'},
-      'b7': {name: 'pawn', color: 'black', symbol: '\u265F'},
-      'c7': {name: 'pawn', color: 'black', symbol: '\u265F'},
-      'd7': {name: 'pawn', color: 'black', symbol: '\u265F'},
-      'e7': {name: 'pawn', color: 'black', symbol: '\u265F'},
-      'f7': {name: 'pawn', color: 'black', symbol: '\u265F'},
-      'g7': {name: 'pawn', color: 'black', symbol: '\u265F'},
-      'h7': {name: 'pawn', color: 'black', symbol: '\u265F'},
-
-      'a8': {name: 'rook', color: 'black', symbol: '\u265C'},
-      'b8': {name: 'knight', color: 'black', symbol: '\u265E'},
-      'c8': {name: 'bishop', color: 'black', symbol: '\u265D'},
-      'd8': {name: 'queen', color: 'black', symbol: '\u265B'},
-      'e8': {name: 'king', color: 'black', symbol: '\u265A'},
-      'f8': {name: 'bishop', color: 'black', symbol: '\u265D'},
-      'g8': {name: 'knight', color: 'black', symbol: '\u265E'},
-      'h8': {name: 'rook', color: 'black', symbol: '\u265C'},
-    };
-    this.chessController = new chess_controller(this.chessDrawer, this.state)
-    this.chessDrawer = new chess_drawer(this.chessController, this.state)
-    this.chessController.drawer = this.chessDrawer
+    for (let i = 0; i < 8; i++) {
+      state[0][i] = {name: namePieces[i], color: 'white', symbol: whitePieces[i]};
+      state[1][i] = {name: 'pawn', color: 'white', symbol: '\u2659'};
+      state[6][i] = {name: 'pawn', color: 'black', symbol: '\u265F'};
+      state[7][i] = {name: namePieces[i], color: 'black', symbol: blackPieces[i]};
+    }
+    state['turn'] = 'white'
+    super(state);
   }
 
-  init(){
-    this.chessDrawer.draw(8, 8)
+  drawer(state) {
+    super.drawer(state)
+    const board = document.getElementById('board')
+    if(board) board.remove()
+
+    const table = document.createElement("div");table.id = 'board'
+    for (let i = 0; i < 8; i++) {
+      const tr = document.createElement('div');
+      for (let j = 0; j < 8; j++) {
+        const td = document.createElement('button');
+        td.className = (i%2 === j%2 ? "white" : "grey")
+        td.style.width = td.style.height = '1.3em'
+        td.style.fontSize = '3em'
+        td.textContent = state[i][j].symbol
+        tr.appendChild(td);
+      }
+      tr.style.display = 'flex'
+      table.appendChild(tr);
+    }
+    document.body.appendChild(table);
+  }
+
+  controller(state, input) {
+    super.controller(state, input);
+    const splitted = input.split(" ")
+    if(splitted.length !== 2) {
+      console.log("Invalid Move!")
+      return
+    }
+
+    const from = splitted[0], to = splitted[1]
+    const rowFrom = from[0], rowTo = to[0]
+    const colFrom = from[1], colTo = to[1]
+
+
+    console.log("Nice move")
   }
 
 }
